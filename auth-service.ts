a7b924db83f3a857e4746ff3d51f501ed3b804b9
ashish21meta@gmail.com <ashish21meta@gmail.com>
2024-12-11 10:52:44 +0530

    1st commit

---
+++ auth-service.ts
@@ -0,0 +1,16 @@
+import start from './tracer';
+start('auth-service');
+import opentelemetry from "@opentelemetry/api";
+import express from 'express';
+const app = express();
+
+app.get('/auth',(req,res)=>{
+    const baggage = opentelemetry.propagation.getActiveBaggage()
+    console.log('baggage',baggage)
+    res.json({username: 'Ashish', userId:123})
+    opentelemetry.trace.getActiveSpan()?.setAttribute('userId',123);
+})
+
+app.listen(8080, () => {
+    console.log('service is up and running!');
+})
---
+++ todo-service.ts
@@ -0,0 +1,90 @@
+import start from './tracer';
+const meter = start('todo-service');
+import express from 'express';
+import axios from 'axios';
+import opentelemetry from "@opentelemetry/api";
+const app = express();
+
+import Redis from "ioredis";
+import { api } from '@opentelemetry/sdk-node';
+const redis = new Redis({ host: 'redis' });
+
+const calls = meter.createHistogram('http-calls');
+
+app.use((req, res, next) => {
+    const startTime = Date.now();
+    req.on('end', () => {
+        const endTime = Date.now();
+        calls.record(endTime - startTime, {
+            route: req.route?.path,
+            status: res.statusCode,
+            method: req.method
+        })
+    })
+    next();
+})
+
+const sleep = (time: number) => { return new Promise((resolve) => { setTimeout(resolve, time) }) };
+
+
+app.get('/todos', async (req, res) => {
+    const baggage = opentelemetry.propagation.createBaggage({
+        "user.plan": {
+            value: "enterprise"
+        }
+    })
+    const contextWithBaggage = opentelemetry.propagation.setBaggage(opentelemetry.context.active(), baggage);
+    opentelemetry.context.with(contextWithBaggage, async () => {
+        const user = await axios.get('http://auth:8080/auth');
+        const todoKeys = await redis.keys('todo:*');
+        const todos: any = [];
+        for (let i = 0; i < todoKeys.length; i++) {
+            const todoItem = await redis.get(todoKeys[i])
+            if (todoItem) {
+                todos.push(JSON.parse(todoItem));
+            }
+        }
+
+        if (req.query['slow']) {
+            await sleep(1000);
+        }
+
+        if (req.query['fail']) {
+            try {
+                throw new Error('Really bad error!')
+            } catch (e: any) {
+                const activeSpan = api.trace.getSpan(api.context.active());
+                activeSpan?.recordException(e)
+                console.error('Really bad error!', {
+                    spanId: activeSpan?.spanContext().spanId,
+                    traceId: activeSpan?.spanContext().traceId,
+                    traceFlag: activeSpan?.spanContext().traceFlags,
+                });
+                res.sendStatus(500);
+                return;
+            }
+        }
+
+        res.json({ todos, user: user.data });
+    })
+
+})
+
+app.listen(8080, () => {
+    console.log('service is up and running!');
+})
+
+
+async function init() {
+    opentelemetry.trace.getTracer('init').startActiveSpan('Set default items', async (span) => {
+        await Promise.all([
+            redis.set('todo:1', JSON.stringify({ name: 'Install OpenTelemetry SDK' })),
+            redis.set('todo:2', JSON.stringify({ name: 'Deploy OpenTelemetry Collector' })),
+            redis.set('todo:3', JSON.stringify({ name: 'Configure sampling rule' })),
+            redis.set('todo:4', JSON.stringify({ name: 'OpenTelemetry master!' }))]
+        );
+        span.end();
+    })
+
+}
+init();
